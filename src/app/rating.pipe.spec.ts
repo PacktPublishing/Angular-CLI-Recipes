2c8dca7617151c3e6374aa4d350335404a94a33b
Orestes Carracedo <orestes@orestes.io>
2019-02-22 08:16:17 +0100

    Add RatingPipe test

--- src/app/rating.pipe.spec.ts
+++ src/app/rating.pipe.spec.ts
@@ -5,4 +5,11 @@ describe('RatingPipe', () => {
     const pipe = new RatingPipe();
     expect(pipe).toBeTruthy();
   });
+  it('rate excellent movies', () => {
+    const pipe = new RatingPipe();
+    const result = pipe.transform(9);
+    const expected = 'excellent';
+
+    expect(result).toEqual(expected);
+  });
 });
