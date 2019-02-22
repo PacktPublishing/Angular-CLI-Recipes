e327d3965af8762f271b6ce434fa9825bd9bee0a
Orestes Carracedo <orestes@orestes.io>
2019-02-22 10:39:51 +0100

    Fix generated e2e test

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to my-shows!');
+    expect(page.getTitleText()).toEqual('My Shows');
   });
 });
